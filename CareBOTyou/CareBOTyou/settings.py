3b6c392178046bedb25962d3804e233ff9d6a380
Azeem Idrisi <azeemidrisi@protonmail.com>
2023-09-24 17:01:22 +0530

    Included MySQL initial setup file

--- CareBOTyou/CareBOTyou/settings.py
+++ CareBOTyou/CareBOTyou/settings.py
@@ -78,7 +78,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'carebotyou',
         'USER': 'root',
-        'PASSWORD': 'YOUR MY SQL PASSWORD',
+        'PASSWORD': 'root',
         'HOST': 'localhost',  # or the hostname where your MySQL server is running
         'PORT': '3306',      # or the port on which your MySQL server is listening
     }
---
+++ mysql_database_settings.sql
@@ -0,0 +1,4 @@
+
+CREATE DATABASE IF NOT EXISTS carebotyou;
+USE carebotyou;
+SELECT * FROM auth_user;
